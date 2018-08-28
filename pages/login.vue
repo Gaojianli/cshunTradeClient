f3b3439b6998be3e69929f8c13d94b0ea4e26efd
dixeran <dixeran@outlook.com>
2018-08-28 22:40:42 +0800

    微调界面

--- pages/login.vue
+++ pages/login.vue
@@ -3,21 +3,20 @@
     <v-layout align-center justify-center>
       <v-flex xs12 sm8 md4>
         <v-card class="elevation-12">
-
           <v-toolbar dark color="primary">
             <v-toolbar-title>登陆系统</v-toolbar-title>
           </v-toolbar>
-
           <v-card-text>
             <v-form>
-              <v-text-field v-model="username" prepend-icon="person" name="login" label="用户名" type="text"></v-text-field>
-              <v-text-field v-model="password" id="password" prepend-icon="lock" name="password"
+              <v-text-field v-model="username" prepend-icon="fas fa-user-circle" name="login" label="用户名"
+                type="text"></v-text-field>
+              <v-text-field v-model="password" id="password" prepend-icon="fas fa-lock" name="password"
                 label="密码" type="password"></v-text-field>
             </v-form>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" @click="login({username,password})">Login</v-btn>
+            <v-btn color="primary" @click="login({username,password})">登陆</v-btn>
           </v-card-actions>
           <v-alert :value="!!loginError" type="error">
             {{loginError}}
@@ -30,9 +29,9 @@
 
 <script>
 import { createNamespacedHelpers } from "vuex";
-const { mapActions, mapGetters,mapState } = createNamespacedHelpers("user");
+const { mapActions, mapGetters, mapState } = createNamespacedHelpers("user");
 export default {
-  computed: {...mapGetters(["isLogin"]),...mapState(["loginError"])},
+  computed: { ...mapGetters(["isLogin"]), ...mapState(["loginError"]) },
   data() {
     return {
       username: "",
@@ -42,6 +41,6 @@ export default {
   methods: {
     ...mapActions(["login"])
   }
-}
+};
 </script>
 
