ee1c291bcaa165d87f1968ae45f965602129294e
dixeran <dixeran@outlook.com>
2018-11-04 15:00:11 +0800

    修复个人信息表单 - fix needed

--- store/user.js
+++ store/user.js
@@ -1,16 +1,17 @@
 export const state = () => ({
-  username: null,
-  id: -1,
-  createdAt: "",
-  name: '',
-  phone: '',
-  street: '',
-  town: '',
-  updatedAt: "",
-  adminLevel: '',
-  isAdmin: false,
-  districtsRW: [],
-  loginError: ""
+  "id": 11,
+  "password": "",
+  "username": "",
+  "first_name": "",
+  "last_name": "",
+  "email": "",
+  "is_active": true,
+  "street": "",
+  "village": "",
+  "phone_number": "",
+  "admin_level": "",
+  "groups": [],
+  "user_permissions": []
 })
 export const getters = {
   isLogin: ({ username }) => !!username,
@@ -24,18 +25,19 @@ export const getters = {
 }
 export const mutations = {
   update(state, user_data) {
-    state.username = user_data.username;
-    state.adminLevel = user_data.adminLevel;
-    state.isAdmin = user_data.adminLevel !== "Normal";
-    state.createdAt = user_data.createdAt;
-    state.name = user_data.name;
-    state.phone = user_data.phone;
-    state.street = user_data.street;
-    state.town = user_data.town;
-    state.updatedAt = user_data.updatedAt;
-    state.districtsRW = user_data.districtsRW;
     state.id = user_data.id;
-    state.loginError = "";
+    state.password = user_data.password;
+    state.username = user_data.username;
+    state.first_name = user_data.first_name;
+    state.last_name = user_data.last_name;
+    state.email = user_data.email;
+    state.is_active = user_data.is_active;
+    state.street = user_data.street;
+    state.village = user_data.village;
+    state.phone_number = user_data.phone_number;
+    state.admin_level = user_data.admin_level;
+    state.groups = user_data.groups;
+    state.user_permissions = user_data.user_permissions;
   },
   updateErrorMessage(state, message) {
     state.loginError = message;
@@ -78,35 +80,21 @@ export const actions = {
    */
   me({ commit }, token) {
     return new Promise((resolve, reject) => {
-      // this.$axios.get(`/users/${token}`, token ? {
-      //     headers: {
-      //       Authorization: `JWT ${token}`
-      //     }
-      //   } : {})
-      //   .then(({ data }) => {
-      //     console.log(data);
-      //     if (token) this.$axios.setToken(token, 'JWT'); //第一次登陆的时候在login里面setToken
-      //     commit('update', data);
-      //     resolve();
-      //   })
-      //   .catch(err => {
-      //     console.log(err);
-      //     reject();
-      //   })
-      let mook = {
-        username: '瞎猜的',
-        adminLevel: 'Super',
-        createdAt: 'today',
-        name: '鬼知道',
-        phone: 123456789,
-        street: '小西天',
-        town: '狮子坟',
-        updatedAt: 'yesterday',
-        districtsRW: '??',
-        id: 0,
-      }
-      commit('update', mook);
-      resolve();
+      this.$axios.get(`/api/users/me`, token ? {
+          headers: {
+            Authorization: `JWT ${token}`
+          }
+        } : {})
+        .then(({ data }) => {
+          console.log(data);
+          if (token) this.$axios.setToken(token, 'JWT'); //第一次登陆的时候在login里面setToken
+          commit('update', data);
+          resolve();
+        })
+        .catch(err => {
+          console.log(err);
+          reject();
+        })
     })
   },
   /**
@@ -115,7 +103,7 @@ export const actions = {
   update({ commit }, new_data) {
     console.log(new_data.id);
     return new Promise((resolve, reject) => {
-      this.$axios.put(`/users/${new_data.id}`, new_data)
+      this.$axios.put(`/api/users/me`, new_data)
         .then(({ data }) => {
           commit('update', data);
           resolve();
